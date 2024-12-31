a965f71c01f485c3d6ab17587b8875b56387bc42
WebM <anglichaneca@gmail.com>
2024-12-31 19:47:02 +0200

    Add files via upload

---
+++ web/js/main.js
@@ -0,0 +1,41 @@
+import { createOptions } from "./createOptions.js";
+
+const optionsWrapper = document.getElementById("options-wrapper");
+const body = document.body;
+const eye = document.getElementById("eyeSvg");
+
+window.addEventListener("message", (event) => {
+  optionsWrapper.innerHTML = "";
+
+  switch (event.data.event) {
+    case "visible": {
+      body.style.visibility = event.data.state ? "visible" : "hidden";
+      body.style.opacity = event.data.state ? "1" : "0";
+      eye.classList.remove("interaction");
+    }
+
+    case "leftTarget": {
+      return eye.classList.remove("eye-hover");
+    }
+
+    case "setTarget": {
+      eye.classList.add("eye-hover");
+
+      if (event.data.options) {
+        for (const type in event.data.options) {
+          event.data.options[type].forEach((data, id) => {
+            createOptions(type, data, id + 1);
+          });
+        }
+      }
+
+      if (event.data.zones) {
+        for (let i = 0; i < event.data.zones.length; i++) {
+          event.data.zones[i].forEach((data, id) => {
+            createOptions("zones", data, id + 1, i + 1);
+          });
+        }
+      }
+    }
+  }
+});
